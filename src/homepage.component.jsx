c6f36f65a470713f8bbf953fb6fec1dae43b431b
Federico De Joan <federico.dejoan@globant.com>
2022-02-21 19:02:05 -0300

    Base create react files + Base homepage files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Chucu</h1>
+          <span className='subtitle'>Mojos</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Chucu2</h1>
+          <span className='subtitle'>Mojos2</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Chucu3</h1>
+          <span className='subtitle'>Mojos3</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Chucu</h1>
+          <span className='subtitle'>Mojos</span>
+        </div>
+      </div>
+    </div>
+
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Chucu4</h1>
+          <span className='subtitle'>Mojos4</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
